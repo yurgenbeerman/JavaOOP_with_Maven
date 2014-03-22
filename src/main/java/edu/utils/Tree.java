d411419c5f547129ffb1b2c82503bc843736740a
yurgenbeerman <yurij.pyvovarenko@gmail.com>
2014-03-22 15:10:27 +0200

    First test is green now.

--- src/main/java/edu/utils/Tree.java
+++ src/main/java/edu/utils/Tree.java
@@ -8,9 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Tree extends ArrayList<TreeNode> {
-    TreeNode rootNode;
-
     public Tree(String rootNodeData) {
-        this.rootNode = new TreeNode(rootNodeData);
+        this.add(new TreeNode(rootNodeData));
     }
 }
