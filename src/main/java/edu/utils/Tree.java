7fcad1a76b76c8b303ad2bfaf2e196106107e6b4
yurgenbeerman <yurij.pyvovarenko@gmail.com>
2014-03-22 15:55:33 +0200

    added more parameters to TreeNode constructor

--- src/main/java/edu/utils/Tree.java
+++ src/main/java/edu/utils/Tree.java
@@ -9,12 +9,14 @@ import java.util.List;
 
 public class Tree extends ArrayList<TreeNode> {
     public Tree(String rootNodeData) {
-        this.add(new TreeNode(rootNodeData));
+        this.add(new TreeNode(rootNodeData, null, null));
     }
 
     public void addNode(String nodeData) {
         //TODO Node constructor should take 4 parameters: parent, left, right and data
         //TODO check to what tree branch add new node -- needs getHeight
-        this.add(new TreeNode(nodeData));
+        this.add(new TreeNode(nodeData, null, null));
     }
+
+
 }
--- src/main/java/edu/utils/TreeNode.java
+++ src/main/java/edu/utils/TreeNode.java
@@ -1,14 +1,23 @@
 package edu.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Lena on 22.03.14.
  */
 public class TreeNode {
     String data;
-    TreeNode ParentNode;
-    //TreeNode ParentNode;
+    TreeNode parentNode;
+    List<TreeNode> childNodes;
 
-    public TreeNode(String data) {
+    public TreeNode(String data, TreeNode parentNode, List<TreeNode> childNodes) {
         this.data = data;
+        this.parentNode = parentNode;
+        this.childNodes = childNodes;
+    }
+
+    public int getHeight() {
+        return 0;
     }
 }
