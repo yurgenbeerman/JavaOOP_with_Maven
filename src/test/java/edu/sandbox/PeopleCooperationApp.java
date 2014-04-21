f4a83c038f06572f20411475ce9485dd4eb6e24b
yurgenbeerman <yurij.pyvovarenko@gmail.com>
2014-04-21 08:42:03 +0300

    added structure for PeopleCooperationApp

---
+++ src/test/java/edu/sandbox/PeopleCooperationApp.java
@@ -0,0 +1,47 @@
+package edu.sandbox;
+
+/**
+ * Created by yurij.pyvovarenko on 21.04.14.
+ */
+public class PeopleCooperationApp {
+    /*
+    Human
+        _SocialInfo_
+        wants to help with
+            _CooperetionElements_
+        looks for
+            _CooperetionElements_
+
+    SocialInfo
+        Name
+        Second Name
+        FacebookProfileURL
+        VKontakteProfileURL
+        Skype
+        Email
+
+    Place
+        anywhere (presence is not required)
+        coordinates
+        Address
+        description of place (i.e. for flash mob)
+
+    CooperetionElements
+        info
+            topic
+            professional level
+        active help (actions)
+            _Place_
+            type of action
+            time of acting
+            periodicity of acting
+        resources (things)
+            _Place_
+            tools
+            equipment
+            materials
+                solid
+                liquid
+
+     */
+}
